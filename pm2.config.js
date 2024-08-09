96bf6c595433602b0cad1dc692c631d02bfed81f
link-finance-platform-admin <linkfinanceplatform@gmail.com>
2024-08-09 16:34:03 +0530

    add runner

---
+++ pm2.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: process.env.APP_NAME,
+      script: 'npm',
+      args: 'start',
+      cwd: process.env.APP_CWD,
+      env: {
+        PORT: process.env.PORT,
+      },
+      instances: 1,
+      autorestart: true,
+    },
+  ],
+};
